3058ce6c41f0440c53c4e382b1ae02de0dd0de7d
Nguyen Hoang Thanh <nhthanh100498@gmail.com>
2023-07-16 17:16:45 +0700

    remove some component

--- src/layouts/auth/layout.js
+++ src/layouts/auth/layout.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
-import NextLink from 'next/link';
-import { Box, Typography, Unstable_Grid2 as Grid } from '@mui/material';
-import { Logo } from 'src/components/logo';
+import PropTypes from "prop-types";
+import NextLink from "next/link";
+import { Box, Typography, Unstable_Grid2 as Grid } from "@mui/material";
+import { Logo } from "src/components/logo";
 
 // TODO: Change subtitle text
 
@@ -12,41 +12,43 @@ export const Layout = (props) => {
     <Box
       component="main"
       sx={{
-        display: 'flex',
-        flex: '1 1 auto'
+        display: "flex",
+        flex: "1 1 auto",
       }}
     >
-      <Grid
-        container
-        sx={{ flex: '1 1 auto' }}
-      >
+      <Grid container sx={{ flex: "1 1 auto" }}>
         <Grid
+          style={{
+            width: "100%",
+            height: "60%",
+            marginTop: "30px",
+          }}
           xs={12}
           lg={6}
           sx={{
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'relative'
+            backgroundColor: "background.paper",
+            display: "flex",
+            flexDirection: "column",
+            position: "relative",
           }}
         >
           <Box
             component="header"
             sx={{
-              left: 0,
+              left: 50,
               p: 3,
-              position: 'fixed',
+              position: "fixed",
               top: 0,
-              width: '100%'
+              width: "100%",
             }}
           >
             <Box
               component={NextLink}
               href="/"
               sx={{
-                display: 'inline-flex',
+                display: "inline-flex",
                 height: 32,
-                width: 32
+                width: 32,
               }}
             >
               <Logo />
@@ -54,58 +56,54 @@ export const Layout = (props) => {
           </Box>
           {children}
         </Grid>
-        <Grid
-          xs={12}
+        {/* <Grid
+          xs={{12}}
           lg={6}
           sx={{
-            alignItems: 'center',
-            background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            '& img': {
-              maxWidth: '100%'
-            }
+            alignItems: "center",
+            background: "radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)",
+            color: "white",
+            display: "flex",
+            justifyContent: "center",
+            "& img": {
+              maxWidth: "100%",
+            },
           }}
         >
-          <Box sx={{ p: 3 }}>
-            <Typography
-              align="center"
-              color="inherit"
-              sx={{
-                fontSize: '24px',
-                lineHeight: '32px',
-                mb: 1
-              }}
-              variant="h1"
-            >
-              Welcome to{' '}
-              <Box
-                component="a"
-                sx={{ color: '#15B79E' }}
-                target="_blank"
-              >
-                Devias Kit
-              </Box>
-            </Typography>
-            <Typography
-              align="center"
-              sx={{ mb: 3 }}
-              variant="subtitle1"
-            >
-              A professional kit that comes with ready-to-use MUI components.
-            </Typography>
-            <img
-              alt=""
-              src="/assets/auth-illustration.svg"
-            />
-          </Box>
-        </Grid>
+          <img
+            style={{ width: "100%", height: "100%", position: "relative" }}
+            alt="bg"
+            src="/assets/logos/wp9424127.webp"
+          />
+          <Typography
+            align="center"
+            color="inherit"
+            sx={{
+              fontSize: "24px",
+              lineHeight: "32px",
+              mb: 1,
+            }}
+            variant="h1"
+            style={{
+              width: "auto",
+              position: "absolute",
+              top: "45%",
+              left: "71%",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            Welcome to{" "}
+            <Box component="a" sx={{ color: "#15B79E" }} target="_blank">
+              Phuong
+            </Box>
+          </Typography>
+        </Grid> */}
       </Grid>
     </Box>
   );
 };
 
 Layout.prototypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
 };
